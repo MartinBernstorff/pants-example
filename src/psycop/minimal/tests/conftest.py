701ffd938b81b02fca7ff67c4ff9dbf622d4892b
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-28 16:02:26 +0200

    chore: cleanup

--- src/psycop/minimal/tests/conftest.py
+++ src/psycop/minimal/tests/conftest.py
@@ -8,11 +8,12 @@ import pytest
 def test_string():
     return "A new test_string!"
 
+
 @pytest.fixture()
 def test_string_from_file():
     # Load test_string from test_string_file.txt
     assert pandas.__version__ != 0
-    
+
     file_path = Path(__file__).parent / "test_string_file.txt"
-    
+
     return file_path.read_text()
