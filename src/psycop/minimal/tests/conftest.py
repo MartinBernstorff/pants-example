af5976aba7e354d42bef8b82fdd4309b29e6da94
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-28 15:53:58 +0200

    misc.

--- src/psycop/minimal/tests/conftest.py
+++ src/psycop/minimal/tests/conftest.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import pandas
 import pytest
 
 
@@ -10,6 +11,8 @@ def test_string():
 @pytest.fixture()
 def test_string_from_file():
     # Load test_string from test_string_file.txt
+    assert pandas.__version__ != 0
+    
     file_path = Path(__file__).parent / "test_string_file.txt"
     
     return file_path.read_text()
